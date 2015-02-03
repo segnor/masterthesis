e5b70fc7ce6353a637abb955ab6e2cb598d33672
segnor <segnor@gmail.com>
2015-02-03 19:38:23 +0100

    /apache_spark/**

---
+++ apache_spark/prototypes/SparkMLLibsPrototype/src/main/scala/com/contexagon/thesis/prototype/AbstractParams.scala
@@ -0,0 +1,34 @@
+package com.contexagon.thesis.prototype
+
+/**
+ * Created by Sascha P. Lorenz on 24/01/15.
+ */
+import scala.reflect.runtime.universe._
+
+abstract class AbstractParams[T: TypeTag] {
+
+  private def tag: TypeTag[T] = typeTag[T]
+
+  /**
+   * Finds all case class fields in concrete class instance, and outputs them in JSON-style format:
+   * {
+   *   [field name]:\t[field value]\n
+   *   [field name]:\t[field value]\n
+   *   ...
+   * }
+   */
+  override def toString: String = {
+    val tpe = tag.tpe
+    val allAccessors = tpe.declarations.collect {
+      case m: MethodSymbol if m.isCaseAccessor => m
+    }
+    val mirror = runtimeMirror(getClass.getClassLoader)
+    val instanceMirror = mirror.reflect(this)
+    allAccessors.map { f =>
+      val paramName = f.name.toString
+      val fieldMirror = instanceMirror.reflectField(f)
+      val paramValue = fieldMirror.get
+      s"  $paramName:\t$paramValue"
+    }.mkString("{\n", ",\n", "\n}")
+  }
+}
